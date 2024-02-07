d797e7c426311521993f16d5d18d4d12c4bf153b
Gary Buckle <buckle.g@mac.com>
2024-02-07 11:57:00 +0100

    section 3 111

---
+++ app/meals/error.js
@@ -0,0 +1,9 @@
+'use client';
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>Oooooops Unable to load data!</h1>
+      <p>Failed to fetch the meal data from the database</p>
+    </main>
+  );
+}
