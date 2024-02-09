db08a0f66858d0e232569a076df01e19d6585afc
Gary Buckle <buckle.g@mac.com>
2024-02-09 10:33:08 +0100

    ERROR:  on  section 3 113

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,5 +1,31 @@
 import React from 'react';
+import classes from './page.module.css';
+import Image from 'next/image';
+import { getMeal } from '@/lib/meals';
 
-export default function MealDetailsPage() {
-  return <h1>Details of a selected meal</h1>;
+export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.mealSlug);
+  console.log(meal);
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by
+            <a href={`mailto:${'EMAIL'}`}>NAME</a>
+          </p>
+          <p className={classes.summary}>SUMMARY</p>
+        </div>
+      </header>
+      <main>
+        <p className={classes.instructions}>
+          dangerouslySetInnerHTML{{ __html: '...' }}
+        </p>
+      </main>
+    </>
+  );
 }
