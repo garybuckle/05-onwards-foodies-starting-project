86a16bcb842b9025db54ea80a127cbff6549f210
Gary Buckle <buckle.g@mac.com>
2024-02-15 14:29:44 +0100

    Section 3 120 incomplete

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,19 +1,9 @@
+'use client';
 import classes from './page.module.css';
 import ImagePicker from '@/components/meals/image-picker';
+import { shareMeal } from '@/lib/actions';
 
 export default function ShareMealPage() {
-  async function sharemeal(formData) {
-    'use server';
-    const meal = {
-      title: formData.get('title'),
-      creator_email: formData.get('email'),
-      summary: formData.get('summary'),
-      instructions: formData.get('instructions'),
-      creator: formData.get('name'),
-      image: formData.get('image'),
-    };
-    console.log(meal);
-  }
   return (
     <>
       <header className={classes.header}>
@@ -23,7 +13,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={sharemeal}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
