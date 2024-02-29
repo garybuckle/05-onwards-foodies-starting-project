59076aa1d22694330b0a47769d52fd65a06672a4
Gary Buckle <buckle.g@mac.com>
2024-02-29 08:24:22 +0100

    section 3 125 incomplete

--- app/meals/share/error.js
+++ app/meals/share/error.js
@@ -3,7 +3,8 @@ export default function Error() {
   return (
     <main className="error">
       <h1>Oh no!!! something is wrong!</h1>
-      <p>Failed to save the meal to the database</p>
+      <p>Failed to create a meal</p>
+      <p>PLease check the database and network connections!</p>
     </main>
   );
 }
