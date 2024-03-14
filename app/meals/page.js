fb5549d666cce6d2a3552f75ddb1f8d034dffca2
Gary Buckle <buckle.g@mac.com>
2024-03-14 07:53:07 +0100

    sectioon 3 app routing completed

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,26 +1,29 @@
 /**
-* app/meals/page.js
-*/
+ * app/meals/page.js
+ */
 
+import classes from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
-import classes from './page.module.css';
-import Link from 'next/link';
-import MealsGrid from '@/components/meals/meals-grid';
-import { getMeals } from '@/lib/meals';
-import { Suspense } from 'react';
+export const metadata = {
+  title: "All Meals",
+  description: "Browse the delicious healthy meals.",
+};
 
-async function Meals(){ 
-  const meals = await getMeals()
-return  <MealsGrid meals={meals} />}
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 
-
-export default  function MealsPage() {
- 
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
         <h1>
-          Delicious, healthy meals{' '}
+          Delicious, healthy meals{" "}
           <span className={classes.highlight}>created by you</span>
         </h1>
         <p>
@@ -32,13 +35,12 @@ export default  function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <Suspense fallback={<p className={classes.loading}>Fetching Meals...</p>} >
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching Meals...</p>}
+        >
           <Meals />
         </Suspense>
-       
-       
       </main>
-
     </>
   );
 }
